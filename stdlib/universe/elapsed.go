6621292fd639bc18932ce450c96c05545749d64b
ewendai <yiwendai99@berkeley.edu>
2019-06-27 16:49:04 -0700

    at least i made progress

--- stdlib/universe/elapsed.go
+++ stdlib/universe/elapsed.go
@@ -2,11 +2,10 @@ package universe
 
 import (
 	"fmt"
-	"github.com/influxdata/flux/interpreter"
-	"math"
-
 	"github.com/influxdata/flux"
+	"github.com/influxdata/flux/arrow"
 	"github.com/influxdata/flux/execute"
+	"github.com/influxdata/flux/interpreter"
 	"github.com/influxdata/flux/plan"
 	"github.com/influxdata/flux/semantic"
 )
@@ -14,13 +13,13 @@ import (
 const ElapsedKind = "elapsed"
 
 type ElapsedOpSpec struct {
-	Columns     []string `json:"columns"`
+	TimeColumn	string	 `json:"timeColumn"`
 }
 
 func init() {
 	elapsedSignature := flux.FunctionSignature(
 		map[string]semantic.PolyType{
-			"columns":     semantic.NewArrayPolyType(semantic.String),
+			"timeColumn":  semantic.String,
 		},
 		nil,
 	)
@@ -43,16 +42,12 @@ func createElapsedOpSpec(args flux.Arguments, a *flux.Administration) (flux.Oper
 
 	spec := new(ElapsedOpSpec)
 
-	if cols, ok, err := args.GetArray("columns", semantic.String); err != nil {
+	if nn, ok, err := args.GetString("timeColumn"); err != nil {
 		return nil, err
 	} else if ok {
-		columns, err := interpreter.ToStringArray(cols)
-		if err != nil {
-			return nil, err
-		}
-		spec.Columns = columns
+		spec.TimeColumn = nn
 	} else {
-		spec.Columns = []string{execute.DefaultValueColLabel}
+		spec.TimeColumn = execute.DefaultTimeColLabel
 	}
 
 	return spec, nil
@@ -68,7 +63,7 @@ func (s *ElapsedOpSpec) Kind() flux.OperationKind {
 
 type ElapsedProcedureSpec struct {
 	plan.DefaultCost
-	Columns     []string `json:"columns"`
+	TimeColumn	string	 `json:"timeColumn"`
 }
 
 func newElapsedProcedure(qs flux.OperationSpec, pa plan.Administration) (plan.ProcedureSpec, error) {
@@ -78,21 +73,18 @@ func newElapsedProcedure(qs flux.OperationSpec, pa plan.Administration) (plan.Pr
 	}
 
 	return &ElapsedProcedureSpec{
-		Columns:     spec.Columns,
+		TimeColumn:  spec.TimeColumn,
 	}, nil
 }
 
 func (s *ElapsedProcedureSpec) Kind() plan.ProcedureKind {
 	return ElapsedKind
 }
+
 func (s *ElapsedProcedureSpec) Copy() plan.ProcedureSpec {
-	ns := new(ElapsedProcedureSpec)
-	*ns = *s
-	if s.Columns != nil {
-		ns.Columns = make([]string, len(s.Columns))
-		copy(ns.Columns, s.Columns)
+	return &ElapsedProcedureSpec{
+		TimeColumn: s.TimeColumn,
 	}
-	return ns
 }
 
 func createElapsedTransformation(id execute.DatasetID, mode execute.AccumulationMode, spec plan.ProcedureSpec, a execute.Administration) (execute.Transformation, execute.Dataset, error) {
@@ -109,14 +101,16 @@ func createElapsedTransformation(id execute.DatasetID, mode execute.Accumulation
 type elapsedTransformation struct {
 	d     execute.Dataset
 	cache execute.TableBuilderCache
-	columns     []string
+
+	timeColumn	string
 }
 
 func NewElapsedTransformation(d execute.Dataset, cache execute.TableBuilderCache, spec *ElapsedProcedureSpec) *elapsedTransformation {
 	return &elapsedTransformation{
 		d:           d,
 		cache:       cache,
-		columns:     spec.Columns,
+
+		timeColumn:  spec.TimeColumn,
 	}
 }
 
@@ -134,4 +128,157 @@ func (t *elapsedTransformation) UpdateProcessingTime(id execute.DatasetID, pt ex
 
 func (t *elapsedTransformation) Finish(id execute.DatasetID, err error) {
 	t.d.Finish(err)
+}
+
+func (t *elapsedTransformation) Process(id execute.DatasetID, tbl flux.Table) error {
+	builder, created := t.cache.TableBuilder(tbl.Key())
+	if !created {
+		return fmt.Errorf("found duplicate table with key: %v", tbl.Key())
+	}
+	cols := tbl.Cols()
+	elapsedvalues := make([]*elapsed, len(cols))
+	for j, c := range cols {
+		found := c.Label == t.timeColumn
+
+		if found {
+			var typ flux.ColType
+			switch c.Type {
+			case flux.TInt, flux.TUInt:
+				typ = flux.TInt
+			case flux.TFloat:
+				typ = flux.TFloat
+			}
+			if _, err := builder.AddCol(flux.ColMeta{
+				Label: c.Label,
+				Type:  typ,
+			}); err != nil {
+				return err
+			}
+			elapsedvalues[j] = newElapsed(j)
+		} else {
+			_, err := builder.AddCol(c)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	firstIdx := 1
+	return tbl.Do(func(cr flux.ColReader) error {
+		l := cr.Len()
+
+		if l != 0 {
+			for j, c := range cols {
+				d := elapsedvalues[j]
+				switch c.Type {
+				case flux.TBool:
+					s := arrow.BoolSlice(cr.Bools(j), firstIdx, l)
+					if err := builder.AppendBools(j, s); err != nil {
+						s.Release()
+						return err
+					}
+					s.Release()
+				case flux.TInt:
+					if d != nil {
+						for i := 0; i < l; i++ {
+							if vs := cr.Ints(j); vs.IsValid(i) {
+								if v, first := d.updateInt(vs.Value(i)); !first {
+									if d.nonNegative && v < 0 {
+										if err := builder.AppendNil(j); err != nil {
+											return err
+										}
+									} else {
+										if err := builder.AppendInt(j, v); err != nil {
+											return err
+										}
+									}
+								}
+							} else if err := builder.AppendNil(j); err != nil {
+								return err
+							}
+						}
+					} else {
+						s := arrow.IntSlice(cr.Ints(j), firstIdx, l)
+						if err := builder.AppendInts(j, s); err != nil {
+							s.Release()
+							return err
+						}
+						s.Release()
+					}
+				case flux.TUInt:
+					if d != nil {
+						for i := 0; i < l; i++ {
+							if vs := cr.UInts(j); vs.IsValid(i) {
+								if v, first := d.updateUInt(vs.Value(i)); !first {
+									if d.nonNegative && v < 0 {
+										if err := builder.AppendNil(j); err != nil {
+											return err
+										}
+									} else {
+										if err := builder.AppendInt(j, v); err != nil {
+											return err
+										}
+									}
+								}
+							} else if err := builder.AppendNil(j); err != nil {
+								return err
+							}
+						}
+					} else {
+						s := arrow.UintSlice(cr.UInts(j), firstIdx, l)
+						if err := builder.AppendUInts(j, s); err != nil {
+							s.Release()
+							return err
+						}
+						s.Release()
+					}
+				case flux.TFloat:
+					if d != nil {
+						for i := 0; i < l; i++ {
+							if vs := cr.Floats(j); vs.IsValid(i) {
+								if v, first := d.updateFloat(vs.Value(i)); !first {
+									if d.nonNegative && v < 0 {
+										if err := builder.AppendNil(j); err != nil {
+											return err
+										}
+									} else {
+										if err := builder.AppendFloat(j, v); err != nil {
+											return err
+										}
+									}
+								}
+							} else if err := builder.AppendNil(j); err != nil {
+								return err
+							}
+						}
+					} else {
+						s := arrow.FloatSlice(cr.Floats(j), firstIdx, l)
+						if err := builder.AppendFloats(j, s); err != nil {
+							s.Release()
+							return err
+						}
+						s.Release()
+					}
+				case flux.TString:
+					s := arrow.StringSlice(cr.Strings(j), firstIdx, l)
+					if err := builder.AppendStrings(j, s); err != nil {
+						s.Release()
+						return err
+					}
+					s.Release()
+				case flux.TTime:
+					s := arrow.IntSlice(cr.Times(j), firstIdx, l)
+					if err := builder.AppendTimes(j, s); err != nil {
+						s.Release()
+						return err
+					}
+					s.Release()
+				}
+			}
+		}
+
+		// Now that we skipped the first row, start at 0 for the rest of the batches
+		firstIdx = 0
+		return nil
+	})
 }
